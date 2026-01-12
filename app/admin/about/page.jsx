20cf36774ed34ff70b843ed1afd7d4bc51c9e800
ManojAvhad13 <manojavhad05@gmail.com>
2026-01-12 21:29:06 +0530

    add admin route

---
+++ app/admin/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = async () => {
+    return (
+
+        <h1>This is admin about data</h1>
+
+    )
+}
+
+export default About;
---
+++ app/admin/layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../globals.css";
+// import Navigation from '@/components/Navigation';
+
+const layout = ({ children }) => {
+    return (
+        <html>
+            <body>
+                {/* <Navigation /> */}
+                {/* <h2>Hi Manoj</h2> */}
+                {children}
+            </body>
+        </html>
+    )
+}
+
+export default layout
