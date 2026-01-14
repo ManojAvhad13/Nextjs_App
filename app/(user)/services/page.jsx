35802eaed4c8c509d03c67e40a68917310a75492
ManojAvhad13 <manojavhad05@gmail.com>
2026-01-14 22:23:18 +0530

    commit

--- app/(user)/services/page.jsx
+++ app/(user)/services/page.jsx
@@ -22,12 +22,95 @@ const page = () => {
                                 Manoj Avhad
                             </h3>
                             <p className='text-sm text-gray-600 text-center mt-2'>
-                                Frontend Developer
+                                Full Stack Developer
                             </p>
                             <p className='text-xs text-gray-500 text-center mt-1'>
                                 React & NextJs
                             </p>
                         </div>
+
+                        {/* Team Member 2 */}
+                        <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg hover:-translate-y-2 transition-all duration-300">
+                            <div className="w-24 h-24 bg-blue-200 rounded-full mx-auto mb-4 flex items-center justify-center">
+                                <Image src="/Dummy_Pr1.jpeg" width={500} height={500} alt='Profile2' className='w-full h-full rounded-full' />
+                            </div>
+                            <h3 className='text-lg font-semibold text-center text-gray-800'>
+                                Aakash Gorase
+                            </h3>
+                            <p className='text-sm text-gray-600 text-center mt-2'>
+                                Frontend Developer
+                            </p>
+                            <p className='text-xs text-gray-500 text-center mt-1'>
+                                React & JS
+                            </p>
+                        </div>
+
+                        {/* Team Member 3 */}
+                        <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg hover:-translate-y-2 transition-all duration-300">
+                            <div className="w-24 h-24 bg-blue-200 rounded-full mx-auto mb-4 flex items-center justify-center">
+                                <Image src="/Dummy_Pr1.jpeg" width={500} height={500} alt='Profile2' className='w-full h-full rounded-full' />
+                            </div>
+                            <h3 className='text-lg font-semibold text-center text-gray-800'>
+                                Bhushya Bhai
+                            </h3>
+                            <p className='text-sm text-gray-600 text-center mt-2'>
+                                Frontend Developer
+                            </p>
+                            <p className='text-xs text-gray-500 text-center mt-1'>
+                                HTML & CSS
+                            </p>
+                        </div>
+
+                        {/* Team Member 4 */}
+                        <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg hover:-translate-y-2 transition-all duration-300">
+                            <div className="w-24 h-24 bg-blue-200 rounded-full mx-auto mb-4 flex items-center justify-center">
+                                <Image src="/Dummy_Pr1.jpeg" width={500} height={500} alt='Profile2' className='w-full h-full rounded-full' />
+                            </div>
+                            <h3 className='text-lg font-semibold text-center text-gray-800'>
+                                Bhushan Harpade
+                            </h3>
+                            <p className='text-sm text-gray-600 text-center mt-2'>
+                                Dot Net Developer
+                            </p>
+                            <p className='text-xs text-gray-500 text-center mt-1'>
+                                ASP Dot Net & Blezer
+                            </p>
+                        </div>
+
+                        {/* Team Member 5 */}
+                        <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg hover:-translate-y-2 transition-all duration-300">
+                            <div className="w-24 h-24 bg-blue-200 rounded-full mx-auto mb-4 flex items-center justify-center">
+                                <Image src="/Dummy_Pr1.jpeg" width={500} height={500} alt='Profile2' className='w-full h-full rounded-full' />
+                            </div>
+                            <h3 className='text-lg font-semibold text-center text-gray-800'>
+                                Aakya Bhai
+                            </h3>
+                            <p className='text-sm text-gray-600 text-center mt-2'>
+                                Backend Developer
+                            </p>
+                            <p className='text-xs text-gray-500 text-center mt-1'>
+                                React & JS
+                            </p>
+                        </div>
+
+                        {/* Team Member 6 Full Size Using Fill True Property */}
+                        <div className="bg-white rounded-lg shadow-md hover:shadow-lg hover:-translate-y-2 transition-all duration-300">
+                            <div className="w-full h-full relative bg-blue-200 rounded-full mx-auto mb-4 flex items-center justify-center">
+                                <Image src="/Dummy_Pr1.jpeg" alt='Profile2' className='w-full h-full rounded-full' fill={true} quality={100} priority={false} />
+                                {/* quality={100} using this property increase image quality & size */}
+                                {/* priority={false} using this property access lazy loading means when user see this image that time loads this image otherwise does not load */}
+                            </div>
+                            {/* <h3 className='text-lg font-semibold text-center text-gray-800'>
+                                Takalu Bhai
+                            </h3>
+                            <p className='text-sm text-gray-600 text-center mt-2'>
+                                Frontend Developer
+                            </p>
+                            <p className='text-xs text-gray-500 text-center mt-1'>
+                                React & JS
+                            </p> */}
+                        </div>
+
                     </div>
                 </div>
             </section>
