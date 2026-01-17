b86266e221e33696c912c2c363aa09d494fe077f
ManojAvhad13 <manojavhad05@gmail.com>
2026-01-17 11:13:45 +0530

    added dynamic route in client component using use hooks

---
+++ app/(user)/emp/[empname]/eposts/[epostid]/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { use } from "react";
+
+const SingleProfilePost = (props) => {
+
+    const user = use(props.params);
+    console.log(user);
+
+    return <h1>user = {user.empname}, PostId : {user.epostid}</h1>
+}
+
+export default SingleProfilePost    
